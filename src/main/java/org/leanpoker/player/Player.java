7c9b78c44cdfef16885bd9284941156945cabe1c
zsombor <apathy.zsombor@gmail.com>
2019-01-14 12:10:52 +0100

    print players

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,10 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
 
 public class Player {
 
@@ -11,6 +14,10 @@ public class Player {
         System.err.println(request.toString());
         JsonObject rootObject = request.getAsJsonObject();
         System.err.println(rootObject.get("tournament_id"));
+        JsonArray players = rootObject.getAsJsonArray("players");
+        System.err.println("players: "+players);
+
+
         return 1000;
     }
 
