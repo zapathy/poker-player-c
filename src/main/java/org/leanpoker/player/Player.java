665a8955cc0948d52c2a16e64fd795eec2837922
zsombor <apathy.zsombor@gmail.com>
2019-01-14 13:57:36 +0100

    ranks print

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,36 +4,32 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import org.jetbrains.annotations.NotNull;
 
 public class Player {
 
     static final String VERSION = "0.4";
 
-    public static int betRequest(JsonElement request) {
-//        System.err.println(request.toString());
+    public static int betRequest(@NotNull JsonElement request) {
+        String ranks = "";
         JsonObject rootObject = request.getAsJsonObject();
-//        System.err.println(rootObject.get("tournament_id"));
         JsonArray players = rootObject.getAsJsonArray("players");
-//        System.err.println("players: " + players);
         for (JsonElement playersElement : players
         ) {
             JsonObject playersObject = playersElement.getAsJsonObject();
-//            System.err.println("current player: "+playersObject);
-//            System.err.println("c. player name"+playersObject.getAsJsonPrimitive("name"));
             if (playersObject.getAsJsonPrimitive("name").getAsString().equals("C")) {
                 JsonElement cardsElement = playersObject.getAsJsonArray("hole_cards");
                 JsonArray cardsArray = cardsElement.getAsJsonArray();
-//                System.err.println("as element: " + cardsElement);
-//                System.err.println("as object: " + cardsObject);
                 for (JsonElement card: cardsArray
                      ) {
                     JsonObject cardAsObject = card.getAsJsonObject();
                     JsonPrimitive rank = cardAsObject.getAsJsonPrimitive("rank");
-                    System.err.println("rank: " + rank);
+//                    System.err.println("rank: " + rank);
+                    ranks+=rank.getAsString();
                 }
             }
         }
-
+        System.err.println(ranks);
 
 
         return 1000;
