d56090c2e545d70a0a91878cef83aef9a53c4482
zsombor <apathy.zsombor@gmail.com>
2019-01-14 13:32:02 +0100

    print card and rank

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,19 +3,36 @@ package org.leanpoker.player;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
-import java.util.ArrayList;
+import com.google.gson.JsonPrimitive;
 
 public class Player {
 
-    static final String VERSION = "0.3" ;
+    static final String VERSION = "0.4";
 
     public static int betRequest(JsonElement request) {
         System.err.println(request.toString());
         JsonObject rootObject = request.getAsJsonObject();
         System.err.println(rootObject.get("tournament_id"));
         JsonArray players = rootObject.getAsJsonArray("players");
-        System.err.println("players: "+players);
+        System.err.println("players: " + players);
+        for (JsonElement playersElement : players
+        ) {
+            JsonObject playersObject = playersElement.getAsJsonObject();
+            if (playersObject.getAsJsonPrimitive("name").equals("C")) {
+                JsonElement cardsElement = playersObject.getAsJsonPrimitive("hole_cards");
+                JsonObject cardsObject = cardsElement.getAsJsonObject();
+                System.err.println("as element: " + cardsElement);
+                System.err.println("as object: " + cardsObject);
+                JsonArray cardsArray = cardsElement.getAsJsonArray();
+                for (JsonElement card: cardsArray
+                     ) {
+                    JsonObject cardAsObject = card.getAsJsonObject();
+                    JsonPrimitive rank = cardAsObject.getAsJsonPrimitive("rank");
+                    System.err.println("rank: " + rank);
+                }
+            }
+        }
+
 
 
         return 1000;
